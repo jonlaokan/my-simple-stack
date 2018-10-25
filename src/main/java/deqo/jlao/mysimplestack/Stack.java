e7cb0a97160f8ae041761303729e5d2cb95d653e
jonlaokan <oneofdays@gmail.com>
2018-10-25 10:46:52 +0200

    doc #2 my-simple-stack-2 Add some comment to Stack

--- src/main/java/deqo/jlao/mysimplestack/Stack.java
+++ src/main/java/deqo/jlao/mysimplestack/Stack.java
@@ -12,27 +12,44 @@ public class Stack implements SimpleStack {
     }
 
     @Override
+    /*
+        Add an element to the stack
+     */
     public void push(Item item) {
         stack.add(0,item);
     }
 
     @Override
+    /*
+        Check it the stack is empty
+     */
     public boolean isEmpty() {
         return stack.isEmpty();
     }
 
+
     @Override
+    /*
+        Get the size of the stack
+     */
     public int getSize() {
         return stack.size();
     }
 
     @Override
+    /*
+        Peek the first element of the stack, without removing it from the stack
+     */
     public Item peek() throws EmptyStackException {
         if (this.isEmpty()) throw new EmptyStackException();
         return stack.get(0);
     }
 
     @Override
+    /*
+        Remove the first elements of the stack
+        @return first element
+     */
     public Item pop() throws EmptyStackException {
         if (this.isEmpty()) throw new EmptyStackException();
         return stack.remove(0);
